d4d222509016874f18c95496cf10d62f69a62e0e
farid <faridmcdato@gmail.com>
2018-06-22 02:53:10 +0800

    Photos are now displayed

--- app/Photo.php
+++ app/Photo.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
+    protected $directory = '/images/';
+
     protected $fillable = ['path'];
+
+    protected function getPathAttribute($photo){
+        return $this->directory . $photo;
+    }
 }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -27,10 +27,10 @@
             @foreach($users as $user)
                 <tr>
                     <td>{{$user->id}}</td>
-                    <td>{{$user->photo_id}}</td>
+                    <td><img height="50" src="{{$user->photo ? $user->photo->path : 'http://via.placeholder.com/50x50'}}" alt=""></td>
                     <td>{{$user->name}}</td>
                     <td>{{$user->email}}</td>
-                    <td>{{$user->role->name}}</td>
+                    <td>{{$user->role ? $user->role->name : 'Roles are not created yet.Please create roles.'}}</td>
                     <td>{{$user->created_at->diffForHumans()}}</td>
                     <td>{{$user->updated_at->diffForHumans()}}</td>
                     <td>
